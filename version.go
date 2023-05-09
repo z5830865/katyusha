2651dfdb983430acaa8b565b88acac097799c0c5
z5830865 <5830865@163.com>
2023-05-10 01:01:02 +0800

    Update version.go

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package katyusha
 
 // VERSION is the version of this module.
-const VERSION = "v0.4.1"
+const VERSION = "v0.0.1"
 
 // AUTHORS is the author list of this module.
 const AUTHORS = "john<john@goframe.org>"
